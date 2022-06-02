1f81d2616479e5fae7b7460bd6b1a50759fcb2a0
ronnenn podolsky <ronnennpodolsky@ronnenns-MacBook-Pro.local>
2022-06-02 16:32:05 +0300

    first jquery

--- app.js
+++ app.js
@@ -1,30 +1,76 @@
-const correctAns = ['B', 'A', 'A', 'A'];
 
-const form = document.querySelector('.quiz-form');
-const result = document.querySelector('.result')
+let todos = ['buy milk', 'do homework']
 
-form.addEventListener('submit', e =>{
-    e.preventDefault();
-    
-    let score = 0;
+// const addForm = document.querySelector('.add')
+// const list = document.querySelector('.todos')
+// const search = document.querySelector('.search input')
+// const clearBtn = document.querySelector('.button-clear')
 
-    const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-    correctAns.forEach((answer, index) => {
-        if (answer === userAns[index]) {
-            score+=(100/(correctAns.length));
-        }
+const addForm = $(".add");
+const list = $(".add");
+const search = $(".add");
+const clearBtn = $(".add");
+
+
+
+const renderTodos = (todos) => {
+    html = ''
+    todos.forEach(element =>{
+        html += `<li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${element}</span>
+                <i class="far fa-trash-alt delete"></i>
+                </li>`
     })
-    scrollTo(0,0);
-    result.classList.remove('d-none');
+    list.innerHTML = html;
+}
 
-    let output = 0;
-    const timer = setInterval(() => {
-        result.querySelector('span').textContent = `${output}%`;
-        if (output === score){
-            clearInterval(timer)
-        }
-        output++;
-    }, 20)
+renderTodos(todos)
 
+addForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    search.value = '';
+    const newTodo = addForm.add.value.trim().toLowerCase();
+    if (newTodo.length){
+        todos.unshift(newTodo);
+        renderTodos(todos);
+        addForm.reset();
+    }else{
+        alert("it's empty!");
+    }
+})
+
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')){
+        
+        e.target.parentElement.remove();
+        console.log(e.target.parentElement)
+
+        var index = todos.indexOf(e.target.value);
+        todos.splice(index, 1);
+    }
+})
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    const filtered = todos.filter(element =>{
+        return element.includes(term)
+    })
+    renderTodos(filtered)
+})
+
+clearBtn.addEventListener('dblclick', e => {
+    e.preventDefault();
+    todos = [];
+    clearBtn.style.background = 'red'
+    clearBtn.style.color = 'white'
+    search.value = ''
+    renderTodos(todos)
+})
+
+
+clearBtn.addEventListener('click', e => {
+    e.preventDefault();
+    clearBtn.style.background = 'red'
+    clearBtn.style.color = 'white'
 })
